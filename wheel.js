f6a910256e618f73d011a9690df42d2f4e7a3560
Twitchinanutshel <breakf117@gmail.com>
2024-02-16 00:31:23 +1100

    second

--- wheel.js
+++ wheel.js
@@ -1,8 +1,14 @@
-const resultContainer = document.querySelector('.result-container');
-const btn = document.getElementById('spin');
-var number = Math.ceil(Math.random() * 5000);
+const resultContainer = document.querySelector('.result-container'); //container for spin
+const btn = document.getElementById('spin'); //btn for .onclick
+let number = Math.ceil(Math.random() * 5000); //number variable for random spin speed
+const arrow = document.querySelector('.arrow'); // arrow variable used for the animation that makes arrow have a 'hitting' effect on the wheel
+
 
 btn.onclick = function() {
     resultContainer.style.transform = 'rotate(' + number + 'deg)';
-    number += Math.ceil(Math.random() * 5000)
-}
+    number += Math.ceil(Math.random() * 5000);
+};
+
+
+
+
