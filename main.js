d79b84c9eb2fafc86ee504376cfbcd077430533b
Twitchinanutshel <breakf117@gmail.com>
2024-02-16 15:34:45 +1100

    made it so when you click on the dom the sidebar closes

--- main.js
+++ main.js
@@ -4,15 +4,27 @@ let number = Math.ceil(Math.random() * 5000); //number variable for random spin 
 const arrow = document.querySelector('.arrow'); // arrow variable used for the animation that makes arrow have a 'hitting' effect on the wheel
 
 
+
 spinBtn.onclick = function() {
     resultContainer.style.transform = 'rotate(' + number + 'deg)';
     number += Math.ceil(Math.random() * 5000);
 };
 
 
+
 const sidebarBtn = document.querySelector('.sidebar-toggle') // for toggling the sidebar
 const sidebar = document.querySelector('.sidebar') // the sidebar
+
+
 
 sidebarBtn.addEventListener('click', function(){
     sidebar.classList.toggle('hidden')
 })
+
+document.addEventListener('click', function(event) {
+    // if the click does not contain the sidebar and the click does not contain the sidebar button and the click is not the spin button click, execute code
+    // the reason why we dont use .contains(sidebar) or .contains(sidebarBtn) is because .contains checks if the element is also a descendant of the parent tag, so it would return false
+    if (!event.target.closest('.sidebar') && !event.target.closest('.sidebar-toggle') && event.target !== spinBtn) {
+        sidebar.classList.add('hidden');
+    }
+});
