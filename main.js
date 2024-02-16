20067adfc719eb075a9188ddc4deaebf92d423ba
Twitchinanutshel <breakf117@gmail.com>
2024-02-17 02:24:22 +1100

    sixth

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const resultContainer = document.querySelector('.result-container'); //container for spin
 const spinBtn = document.getElementById('spin'); //btn for .onclick
-let number = Math.ceil(Math.random() * 5000); //number variable for random spin speed
+let number = Math.ceil(Math.random() * 5000 + 3000); //number variable for random spin speed
 const arrow = document.querySelector('.arrow'); // arrow variable used for the animation that makes arrow have a 'hitting' effect on the wheel
+const sidebarImage = document.getElementById('sidebar-i')
 
 
 
 spinBtn.onclick = function() {
     resultContainer.style.transform = 'rotate(' + number + 'deg)';
-    number += Math.ceil(Math.random() * 5000);
+    number += Math.ceil(Math.random() * 5000 + 3000);
 };
 
 
@@ -19,6 +20,7 @@ const sidebar = document.querySelector('.sidebar') // the sidebar
 
 sidebarBtn.addEventListener('click', function(){
     sidebar.classList.toggle('hidden')
+    sidebarImage.classList.toggle('sidebarTran')
 })
 
 document.addEventListener('click', function(event) {
@@ -26,5 +28,6 @@ document.addEventListener('click', function(event) {
     // the reason why we dont use .contains(sidebar) or .contains(sidebarBtn) is because .contains checks if the element is also a descendant of the parent tag, so it would return false
     if (!event.target.closest('.sidebar') && !event.target.closest('.sidebar-toggle') && event.target !== spinBtn) {
         sidebar.classList.add('hidden');
+        sidebarImage.classList.remove('sidebarTran')
     }
 });
